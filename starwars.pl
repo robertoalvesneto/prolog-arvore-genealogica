7c20b716fd57b4c11835274a8aded7372104633f
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-26 20:24:46 -0400

    #Fix : Add quotation marks in all names

--- starwars.pl
+++ starwars.pl
@@ -1,45 +1,45 @@
-pai(cliegg lars,anakin skywalker).
-mae(shmi skywalker, anakin skywalker).
-casados(anakin skywalker, padme amidala).
-casados(leia skywalker,han solo).
-pai(anakin skywalker, luke skywalker).
-pai(anakin skywalker, leia skywalker).
-mae(padme amidala, luke skywalker).
-mae(padme amidala, leia skywalker).
+pai("cliegg lars","anakin skywalker").
+mae("shmi skywalker"," anakin skywalker").
+casados("anakin skywalker"," padme amidala").
+casados("leia skywalker","han solo").
+pai("anakin skywalker"," luke skywalker").
+pai("anakin skywalker"," leia skywalker").
+mae("padme amidala"," luke skywalker").
+mae("padme amidala"," leia skywalker").
 
 
-jedi(luke skywalker).
-jedi(yoda).
-jedi(qui gon jinn).
-jedi(mace windu).
-jedi(obi wan kenobi).
+jedi("luke skywalker").
+jedi("yoda").
+jedi("qui gon jinn").
+jedi("mace windu").
+jedi("obi wan kenobi").
 
 
-killed(palpatine, mace windu).
-killed(darth vader, obi wan).
-killed(darth vader, palpatine).
-killed(darth maul, quin gon jinn).
+killed("palpatine"," mace windu").
+killed("darth vader"," obi wan").
+killed("darth vader"," palpatine").
+killed("darth maul"," quin gon jinn").
 
-apprentice(luke skywalker).
-apprendice(obi wan kenobi).
-apprentice(anakin skywalker).
+apprentice("luke skywalker").
+apprendice("obi wan kenobi").
+apprentice("anakin skywalker").
 
-master(yoda).
-master(quin gon jinn).
-master(obi wan).
+master("yoda").
+master("quin gon jinn").
+master("obi wan").
 
-changed_side(dookan).
-changed_side(anakin skywalker).
-changed_side(jacen solo).
+changed_side("dookan").
+changed_side("anakin skywalker").
+changed_side("jacen solo").
 
 % Sith
-master("Darth Plagueis", "Darth Sidious").
-master("Darth Sidious", "Darth Maul").
-master("Darth Sidious", "Darth Tyranus").
-master("Darth Sidious", "Darth Vader"). 
+master(""Darth Plagueis""," "Darth Sidious"").
+master(""Darth Sidious""," "Darth Maul"").
+master(""Darth Sidious""," "Darth Tyranus"").
+master(""Darth Sidious""," "Darth Vader""). 
 
-masterWho(X,Y):- master(X), apprentice(Y).
+masterWho("X","Y"):- master("X")"," apprentice("Y").
 
 %Changed sides
-turnedWho("Anakin Skywalker", "Darth Vader").
-turnedWho("Conde Dookan", "Darth Tyranus").
+turnedWho(""Anakin Skywalker""," "Darth Vader"").
+turnedWho(""Conde Dookan""," "Darth Tyranus"").
